ed360dd45c7af854fa5fd1daeca8937890d396b0
Azvi01 <javierdiazlopez00@gmail.com>
2026-01-23 11:46:24 +0100

    progreso de attr de producto

--- tomaJeroma/app/models/Attributes.php
+++ tomaJeroma/app/models/Attributes.php
@@ -5,5 +5,8 @@
         public $value;
 
         public function getId() { return $this->id; }
+        public function getAttr_name() { return $this->attr_name; }
+        public function getValue() { return $this->value; }
+        
     }
 ?>
